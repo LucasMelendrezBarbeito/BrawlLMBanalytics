416f0a78323625870a52ccfd0ca8a0d5f4b1ad82
LucasMelendrez <Lucasmb20013@gmail.com>
2026-05-19 19:07:59 +0200

    getters setter y constructores sin lombok dtos

--- src/main/java/com/brawllmbanalytics/dto/VincularCuentaRequest.java
+++ src/main/java/com/brawllmbanalytics/dto/VincularCuentaRequest.java
@@ -1,13 +1,40 @@
 package com.brawllmbanalytics.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
 public class VincularCuentaRequest {
     private Integer usuarioId;
     private String tag;
     private String nombre;
     private Integer trofeos;
     private Integer nivel;
+    public Integer getUsuarioId() {
+        return usuarioId;
+    }
+    public void setUsuarioId(Integer usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    public String getTag() {
+        return tag;
+    }
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Integer getTrofeos() {
+        return trofeos;
+    }
+    public void setTrofeos(Integer trofeos) {
+        this.trofeos = trofeos;
+    }
+    public Integer getNivel() {
+        return nivel;
+    }
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
 }
